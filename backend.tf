f86774aa67ad636292fc9e2a17df440b9b4c5de7
yashingle551 <yingle360@gmail.com>
2026-01-25 17:23:12 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,10 +2,11 @@
 terraform {
  required_version = ">= 1.5.0"
   backend "s3" {
-    bucket         = "sid28jenkinsmumbai"
-    key            = "project1/terraform.tfstate"
+    bucket         = "dipeshdevops4stroage "
+    key            = "yash/terraform.tfstate"
     region         = "eu-north-1"
   }
 }
+
 
 
