e0e21964d0dda835b31eec2dc9c9cc7b126e5c3e
yashingle551 <yingle360@gmail.com>
2026-01-25 17:24:39 +0530

    Update AWS provider version in provider.tf

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "6.28.0"
+      version = "1.5.0"
     }
   }
 }
@@ -12,3 +12,4 @@ provider "aws" {
   region = "eu-north-1"
 
 }
+
